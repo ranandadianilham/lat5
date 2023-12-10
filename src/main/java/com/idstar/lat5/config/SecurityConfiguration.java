e7180cd005fae4a316e1ff2f2a814788a78b1d40
Rananda Dian Ilham <=>
2023-12-10 21:59:43 +0700

    e

--- src/main/java/com/idstar/lat5/config/SecurityConfiguration.java
+++ src/main/java/com/idstar/lat5/config/SecurityConfiguration.java
@@ -4,11 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SuppressWarnings("all")
 @Configuration
@@ -19,21 +19,73 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/v1/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                //.oauth2ResourceServer().jwt()
+                .oauth2Login(Customizer.withDefaults());
+                //.formLogin(Customizer.withDefaults());
+
+
+        /*normal jwt token login*/
+        /*
+
         http
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/v1/auth/**")
-                .permitAll()
-                .anyRequest()
+                //.permitAll()
+                //.anyRequest()
                 .authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);*/
         return http.build();
     }
+
+    /*@Bean
+    public OAuth2AuthorizationRequestRedirectFilter oAuth2Filter() {
+        return new OAuth2AuthorizationRequestRedirectFilter(
+                new BaseUriRequestMatcher("/oauth2/authorization", null),
+                authorizationRequestResolver());
+    }
+
+    @Bean
+    public OAuth2AuthorizationRequestResolver authorizationRequestResolver() {
+        return new DefaultOAuth2AuthorizationRequestResolver(
+                clientRegistrationRepository(),
+                "/oauth2/authorization");
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        // Configure your OAuth2 client registration details here
+        return new InMemoryClientRegistrationRepository(
+                ClientRegistration
+                        .withRegistrationId("your-registration-id")
+                        .clientId("your-client-id")
+                        .clientSecret("your-client-secret")
+                        .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
+                        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                        .redirectUriTemplate("{baseUrl}/login/oauth2/code/{registrationId}")
+                        .scope("openid", "profile", "email")
+                        .authorizationUri("your-authorization-uri")
+                        .tokenUri("your-token-uri")
+                        .userInfoUri("your-user-info-uri")
+                        .userNameAttributeName(IdTokenClaimNames.SUB)
+                        .jwkSetUri("your-jwk-set-uri")
+                        .clientName("your-client-name")
+                        .build());
+    }*/
 }
